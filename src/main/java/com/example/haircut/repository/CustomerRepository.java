03e2b1e9d9dfdb5d7723b5fcf39a061a1ce9c349
Kwang Ming <kwangming220500@gmail.com>
2021-10-03 14:08:47 +0700

    test

--- src/main/java/com/example/haircut/repository/CustomerRepository.java
+++ src/main/java/com/example/haircut/repository/CustomerRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface CustomerRepository extends MongoRepository<Customer,String> {
     Optional<Customer> findCustomerByCusEmail(String cusEmail);
-//    Optional<Customer> findCustomerByCusEmailAndPassword(String cusEmail, String passworrd);
+    Optional<Customer> findCustomerByCusEmailAndPassword(String cusEmail, String passworrd);
 
 }

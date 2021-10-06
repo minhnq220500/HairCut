cb6a67125f20ceac845942659ea5549a61c2efa3
Kwang Ming <kwangming220500@gmail.com>
2021-10-06 21:15:38 +0700

    Update HairCutApplication.java

--- src/main/java/com/example/haircut/HairCutApplication.java
+++ src/main/java/com/example/haircut/HairCutApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HairCutApplicationn {
+public class HairCutApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HairCutApplicationn.class, args);
+        SpringApplication.run(HairCutApplication.class, args);
     }
 
 }

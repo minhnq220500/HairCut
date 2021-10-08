e092366f89d3852e80b958478ad7d62d69e02c86
Kwang Ming <kwangming220500@gmail.com>
2021-10-08 13:11:32 +0700

    Update HairCutApplication.java

--- src/main/java/com/example/haircut/HairCutApplication.java
+++ src/main/java/com/example/haircut/HairCutApplication.java
@@ -2,12 +2,29 @@ package com.example.haircut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HairCutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HairCutApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500",
+                                "https://haircut-fall-2021.herokuapp.com",
+                                "http://localhost:8080");
+            }
+        };
     }
 
 }

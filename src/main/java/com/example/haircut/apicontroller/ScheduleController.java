10255f11f767579aff5f51bbe20f888d3da14a3a
truong0145 <66652065+truong0145@users.noreply.github.com>
2021-10-28 14:34:41 +0700

    add update schedule status, get list emp by schlid

--- src/main/java/com/example/haircut/apicontroller/ScheduleController.java
+++ src/main/java/com/example/haircut/apicontroller/ScheduleController.java
@@ -1,6 +1,8 @@
 package com.example.haircut.apicontroller;
 
+import com.example.haircut.model.Employee;
 import com.example.haircut.model.Schedule;
+import com.example.haircut.repository.EmployeeRepository;
 import com.example.haircut.repository.ScheduleRepository;
 import com.example.haircut.utils.MyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,31 @@ public class ScheduleController {
     @Autowired
     ScheduleRepository scheduleRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     @GetMapping("/schedules")
     public ResponseEntity<List<Schedule>> getAllSchedule(){
         try{
             List<Schedule> schedules = new ArrayList<>();
             scheduleRepository.findAll().forEach(schedules::add);
+
+            if(!schedules.isEmpty()){
+                return new ResponseEntity<>(schedules, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/availableSchedules")
+    public ResponseEntity<List<Schedule>> getAllScheduleAvailable(){
+        try{
+            List<Schedule> schedules = new ArrayList<>();
+            scheduleRepository.findByStatus(true).forEach(schedules::add);
 
             if(!schedules.isEmpty()){
                 return new ResponseEntity<>(schedules, HttpStatus.OK);
@@ -89,6 +111,35 @@ public class ScheduleController {
 
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updateScheduleStatus")
+    public ResponseEntity<Schedule> updateScheduleStatus(@RequestBody Schedule schedule){
+        try{
+
+            Optional<Schedule> scheduleData = scheduleRepository.findByScheduleID(schedule.getScheduleID());
+
+            List<Employee> employees = new ArrayList<>();
+            employeeRepository.findByScheduleID(schedule.getScheduleID()).forEach(employees::add);
+
+            if(scheduleData.isPresent()){
+
+                Schedule _schedule = scheduleData.get();
+                if(employees.size() == 0){
+                    boolean newStatus = !_schedule.isStatus();
+                    _schedule.setStatus(newStatus);
+                    scheduleRepository.save(_schedule);
+                    return new ResponseEntity<>(_schedule, HttpStatus.OK);
+                }else{
+                    return new ResponseEntity<>(HttpStatus.CONFLICT);
+                }
+            }
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            System.out.println(e.toString().toUpperCase());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }

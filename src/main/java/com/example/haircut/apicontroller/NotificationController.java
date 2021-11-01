d203127cf4e802fc4e744420d5c3ea819477f34e
Kwang Ming <kwangming220500@gmail.com>
2021-11-01 15:48:59 +0700

    Update NotificationController.java

--- src/main/java/com/example/haircut/apicontroller/NotificationController.java
+++ src/main/java/com/example/haircut/apicontroller/NotificationController.java
@@ -30,7 +30,7 @@ public class NotificationController {
             else{
                 List<Notification> listNotiFalse=new ArrayList<>();
                 for (Notification noti:listNoti) {
-                    if(noti.isRead()==false){
+                    if(noti.isRead()==false && noti.getCurrentStatus()=="ACCEPT" || noti.getCurrentStatus()=="CANCEL BY ADMIN"){
                         listNotiFalse.add(noti);
                     }
                 }
